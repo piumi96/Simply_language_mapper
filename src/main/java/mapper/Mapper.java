f0225b216aa280ec89633cc3dbdf130e5251a3ab
Piumi Perera <piumidp96@gmail.com>
2020-11-15 19:44:02 +0530

    Keywords replaced and write translated code to file
    Signed-off-by: Piumi Perera <piumidp96@gmail.com>

--- src/main/java/mapper/Mapper.java
+++ src/main/java/mapper/Mapper.java
@@ -1,12 +1,8 @@
 package mapper;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Map;
 import org.yaml.snakeyaml.Yaml;
-
 
 public class Mapper {
     private Map<String, String> map;
@@ -19,35 +15,71 @@ public class Mapper {
         return this.map;
     }
 
-    public static void getYaml() {
+    public static Map<String, String> getYaml() {
         String path = "src/main/java/mappings/Sinhala.yml";
         File mapFile = new File(path);
         Yaml yaml = new Yaml();
+        Map<String, String> map = null;
+
         try {
             InputStream inputStream = new FileInputStream(mapFile);
-            Map<String, String> map = yaml.load(inputStream);
+            map = yaml.load(inputStream);
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+        return map;
+    }
 
+    public static void mapFile(Map<String, String> map, String lang) throws IOException {
+        String path = "src/main/java/simply/code/testSn.simply";
+        File codeFile = new File(path);
+        File targetFile = new File("src/main/java/simply/code/output.simply");
 
-        /*InputStream inputStream;
+        BufferedReader reader = new BufferedReader(new FileReader(codeFile));
+        FileWriter writer = new FileWriter(targetFile);
+        String content = "";
+        String line = reader.readLine();
 
-        try {
-            inputStream = new FileInputStream(mapFile);
-            Map<String, String> map = yaml.load(inputStream);
-            System.out.println(map);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+        while (line != null)
+        {
+            content += line + "\n";
+            line = reader.readLine();
         }
-*/
 
+        System.out.println(content);
+
+        for(Map.Entry<String, String> entry : map.entrySet()){
+            if(lang != "eng"){
+                content = content.replaceAll(entry.getKey(), entry.getValue());
+            }
+            else if(lang == "eng"){
+                content = content.replaceAll(entry.getValue(), entry.getKey());
+            }
+
+        }
+
+        System.out.println(content);
+        writer.write(content);
+
+        reader.close();
+        writer.close();
 
     }
 
     public static void main(String[] args) {
         Mapper mapper = new Mapper();
-        mapper.getYaml();
+        String lang = "eng";
+
+        //Retrieve Sinhala.yaml file from mappings
+        Map<String, String> map = mapper.getYaml();
+        mapper.setMap(map);
+
+        try {
+            mapper.mapFile(map, lang);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
